4fd93ba3787fd889a6939b33930ca00c11ba06d3
Alvaro Marenco <alvaromarencom@gmail.com>
2025-10-28 17:32:05 -0600

    setSlider function

---
+++ index.js
@@ -0,0 +1,25 @@
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+let carousel = document.querySelector('.carousel');
+let items = carousel.querySelectorAll('.list .item');
+let indicator = carousel.querySelector('.indicators');
+let dots = indicator.querySelectorAll('ul li');
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
+
+const setSlider = () => {
+    let itemActiveOld = carousel.querySelector('.list .item.active');
+    if (itemActiveOld) {
+        itemActiveOld.classList.remove('active');
+    }
+    items[active].classList.add('active');
+
+    let dotActiveOld = indicator.querySelector('ul li.active');
+    if (dotActiveOld) {
+        dotActiveOld.classList.remove('active');
+    }
+    dots[active].classList.add('active');
+    indicator.querySelector('numbers').innerText = '0' + (active+1);
+}
+setSlider();
